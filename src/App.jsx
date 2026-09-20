0d2bb02190301d8a7b881ab3e3bb61c41fc47fa4
Tanmay <rajb55595@gmail.com>
2026-09-20 20:01:12 +0530

    Refactor App component to dynamically render Card components using an array; removed hardcoded instances for improved scalability and maintainability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Card } from './components/Card';
 
+
+
+
 export function App() {
+  let arr=[10,20,30,40]
+  
   return (
     <div style={{display: 'flex', gap: '20px', justifyContent: 'center',  height: '100vh'}}>
       <Card name="Tanmay" im="https://avatars.githubusercontent.com/u/311799795?v=4&size=64" role="Web dev" des="Hi My name is Tanmay"/>
-      <Card/>
-      
+     {arr.map(function(a){
+      return <Card name={a}/>
+
+     }) }
+     
     </div>
   )
 }
