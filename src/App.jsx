68248d0ae020bea127cf8d6867d843e5807fc7b6
Tanmay <rajb55595@gmail.com>
2026-09-25 11:52:49 +0530

    Refactor App component to implement a counter feature using useState; commented out previous Card rendering logic for future reference.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
+import { useState } from 'react';
 import { Card } from './components/Card';
 
 
-
-
 export function App() {
-  let arr=[10,20,30,40]
+  /* let arr=[10,20,30,40] */
   
-  return (
-    <div style={{display: 'flex', gap: '20px', justifyContent: 'center',  height: '100vh'}}>
-      <Card name="Tanmay" im="https://avatars.githubusercontent.com/u/311799795?v=4&size=64" role="Web dev" des="Hi My name is Tanmay"/>
-     {arr.map(function(a){
-      return <Card name={a}/>
+  /* <{div style={{display: 'flex', gap: '20px', justifyContent: 'center',  height: '100vh'}}>
+    <Card name="Tanmay" im="https://avatars.githubusercontent.com/u/311799795?v=4&size=64" role="Web dev" des="Hi My name is Tanmay"/>
+   {arr.map(function(a){
+    return <Card name={a}/>
 
-     }) }
-     
-    </div>
-  )
+   }) }
+   
+  </div> }*/
+  
+let [a,setA]=useState(10);
+function Counter(){
+  setA(a++);
+}
+
+
+
+  return <div>
+     <button onClick={Counter} className='h-10 w-10 bg-gray-400 '>click here</button>
+     <h1>The value is {a}</h1>
+  </div>
+
+
+
 }
